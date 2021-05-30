71b218209dd256d15b6652096edfa381bfbf819e
Vladimir <cobalt1203@gmail.com>
2021-05-30 19:03:29 +0400

    homework3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 import json
+from mainapp.models import ProductCategory
 
 links_menu = [
         {'href': 'main', 'name': 'домой'},
         {'href': 'contact', 'name': 'контакты'},
-        {'href': 'products', 'name': 'продукты'},
+        {'href': 'products:index', 'name': 'продукты'},
 ]
 with open('same_products.json', encoding="utf-8") as json_file:
     same_products = json.load(json_file)
 
 
-def products(request):
+def products(request, pk=None):
+    print(pk)
     title = 'каталог/продукты'
+    category = ProductCategory.objects.all()
     context = {
         'links_menu': links_menu,
         'same_products': same_products,
+        'category': category,
         'title': title
     }
     return render(request, 'mainapp/products.html', context=context)
