046ae3fb6e6fbaa6b7779bb6ea710269562e0e41
Vladimir <cobalt1203@gmail.com>
2021-06-08 20:29:16 +0400

    homework5

--- geekshop/views.py
+++ geekshop/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+
+from basketapp.models import Basket
 from mainapp.models import Product
 
 
-links_menu = [
+main_menu = [
         {'href': 'main', 'name': 'домой'},
         {'href': 'contact', 'name': 'контакты'},
         {'href': 'products:index', 'name': 'продукты'},
@@ -18,19 +20,27 @@ main_products = [
 def main(request):
     title = 'главная'
     main_products_base = Product.objects.all()
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     context = {
-        'links_menu': links_menu,
+        'main_menu': main_menu,
         'main_products': main_products_base,
         'title': title,
+        'basket': basket,
     }
     return render(request, 'geekshop/index.html', context=context)
 
 
 def contact(request):
     title = 'контакты'
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     context = {
-        'links_menu': links_menu,
+        'main_menu': main_menu,
         'title': title,
+        'basket': basket,
     }
     return render(request, 'geekshop/contact.html', context=context)
