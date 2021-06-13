de5d54508229be17e6be360d93780483bc376068
Vladimir <cobalt1203@gmail.com>
2021-06-13 13:25:09 +0400

    homework6

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,26 +1,46 @@
-from django.shortcuts import render
 import json
-from mainapp.models import ProductCategory, Product
-from basketapp.models import Basket
+import random
+
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+from basketapp.models import Basket
+from mainapp.models import ProductCategory, Product
 
 main_menu = [
         {'href': 'main', 'name': 'домой'},
         {'href': 'contact', 'name': 'контакты'},
         {'href': 'products:index', 'name': 'продукты'},
 ]
-with open('same_products.json', encoding="utf-8") as json_file:
-    same_products = json.load(json_file)
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return Basket.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+
+    return random.sample(list(products), 1)[0]
+
+
+def get_same_products(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).exclude(pk=hot_product.pk)[:3]
+
+    return same_products
 
 
 def products(request, pk=None):
 
     title = 'каталог/продукты'
     links_menu = ProductCategory.objects.all()
+    basket = get_basket(request.user)
 
-    basket = []
-    if request.user.is_authenticated:
-        basket = Basket.objects.filter(user=request.user)
+    hot_product = get_hot_product()
+    same_products = get_same_products(hot_product)
 
     if pk is not None:
         if pk == 0:
@@ -43,8 +63,23 @@ def products(request, pk=None):
     context = {
         'main_menu': main_menu,
         'links_menu': links_menu,
+        'hot_product': hot_product,
         'same_products': same_products,
         'title': title,
         'basket': basket,
     }
     return render(request, 'mainapp/products.html', context=context)
+
+
+def product(request, pk):
+    title = 'продукты'
+
+    content = {
+        'title': title,
+        'main_menu': main_menu,
+        'links_menu': ProductCategory.objects.all(),
+        'product': get_object_or_404(Product, pk=pk),
+        'basket': get_basket(request.user),
+    }
+
+    return render(request, 'mainapp/product.html', content)
